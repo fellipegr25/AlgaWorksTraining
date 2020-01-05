80c4a71964f9a55439a3cb3c9fd9b9bb5cd78934
felliperibeiro <felliperibeiro@mbp-de-fellipe.lan>
2020-01-04 21:46:24 -0300

    First commit

---
+++ CursoLogicaCapitulo08/src/Exercicio01v1.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Exercicio01v1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		imprimirTraco();
+		
+		String[] cursos = new String[] {"JavaEE", "Spring", "Java OO Avançado"};
+		
+		System.out.println("Escolha dentre os cursos abaixo: ");
+		
+		for (int i = 0; i < cursos.length; i++) {
+			System.out.println("[" + i + "]" + cursos[i]);
+		}
+		
+		System.out.print("O curso que você deseja é o: ");
+		Integer posicaoCursoEscolhido = scanner.nextInt();
+		
+		Boolean posicaoValida = posicaoCursoEscolhido >= 0 && posicaoCursoEscolhido < cursos.length;
+		
+		if (!posicaoValida) {
+			encerraPrograma();
+		}
+		
+		imprimirTraco();
+		
+		String[] formasPagamento = new String[] {"Cartão", "Boleto"};
+		
+		System.out.println("Escolha dentre as formas de pagamento abaixo: ");
+		
+		for (int i = 0; i < formasPagamento.length; i++) {
+			System.out.println("[" + i + "]" + formasPagamento[i]);
+		}
+		
+		System.out.print("Sua forma de pagamento escolhida é: ");
+		Integer posicaoFormaDePagamentoEscolhida = scanner.nextInt();
+		
+		posicaoValida = posicaoFormaDePagamentoEscolhida >= 0 
+				&& posicaoFormaDePagamentoEscolhida < formasPagamento.length;
+		
+		if (!posicaoValida) {
+			encerraPrograma();
+		}
+		
+		String cursoEscolhido = cursos[posicaoCursoEscolhido];
+		String formaPagamentoEscolhido = formasPagamento[posicaoFormaDePagamentoEscolhida];
+		
+		imprimirTraco();
+		
+		System.out.println("O curso escolhido foi " + cursoEscolhido + " e a forma de pagamento é " + formaPagamentoEscolhido);
+		
+		scanner.close();
+		
+	}
+
+	static void imprimirTraco() {
+		System.out.println("----------------------------------------");
+	}
+	
+	static void encerraPrograma() {
+		System.err.println("Posição inválida!");
+		System.exit(1);
+	}
+	
+}
