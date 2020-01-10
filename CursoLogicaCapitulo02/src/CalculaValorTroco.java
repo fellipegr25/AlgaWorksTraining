1e33606db5f81fff946faa08972c1364aa71aec7
felliperibeiro <felliperibeiro@mbp-de-fellipe.lan>
2020-01-10 03:35:14 -0300

    First commit

--- CursoLogicaCapitulo02/src/CalculaValorTroco.java
+++ CursoLogicaCapitulo02/src/CalculaValorTroco.java
@@ -15,15 +15,6 @@ public class CalculaValorTroco {
 		
 		interacao.fechar();
 		
-//		System.out.print();
-//		Double valorProduto = scanner.nextDouble();
-//		
-//		System.out.print("Digite o valor passado pelo cliente: ");
-//		Double valorPassadoPeloCliente = scanner.nextDouble();
-//		
-//		Double valorTroco = valorPassadoPeloCliente - valorProduto;
-//		
-//		System.out.println("Valor do troco: R$" + valorTroco);
-		
+
 	}
 }
